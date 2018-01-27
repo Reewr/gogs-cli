8f476dae5e86c1b79260971e2ded7ed1cc26ae88
Reewr <alexander.imenes@gmail.com>
2018-01-27 22:10:41 +0100

    Converts to using promise handler
    This will make it far easier to perform testing

--- commands/repo-commands/list.js
+++ commands/repo-commands/list.js
@@ -1,28 +1,16 @@
 'use strict';
 const request = require('../../lib/request');
+const handler = require('../../lib/handler');
 
 module.exports = {
-  command: 'repo',
-  desc   : `
-This command can be used to list all available repositories
-
-Usage:
-  gogs repo list
-`,
+  command: 'list',
+  desc   : 'List all available repositories',
+  example: 'gogs repo list',
   builder: {},
-  handler: () => {
-    const res = request.get(`/user/repos`);
+  handler: handler.mkHandler(async function() {
+    const res = request.get('/user/repos');
 
-    res.on('error', (err) => {
-      console.error('Failed to retrieve issues due to error: ', err);
-    });
-
-    res.on(400, () => console.error('The access token you used does not have access'));
-    res.on(401, () => console.error('The request that was made was bad'));
-    res.on(404, () => console.error('Could not find the requested resources'));
-    res.on(500, () => console.error('An internal server error happened with Gogs'));
-    res.on('success', repos => {
-      console.log(repos.map(x => x.full_name).join('\t'));
-    });
-  }
+    return res.waitForSuccess()
+      .then((repos) => repos.map(x => x.full_name).join('\t'));
+  })
 };
