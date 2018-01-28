dd482bf45e3bd0f7b8730b1e1498a554fe98296f
Reewr <alexander.imenes@gmail.com>
2018-01-28 18:38:40 +0100

    Only logs message if any

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,10 @@ const gogs = require('../index');
 const GogsCliError = require('../lib/errors').GogsCliError;
 
 gogs(process.argv.slice(2))
-  .then((msg) => console.log(msg))
+  .then((msg) => {
+    if (msg)
+      console.log(msg);
+  })
   .catch(err => {
     if (err instanceof GogsCliError) {
       console.error(err.message);
