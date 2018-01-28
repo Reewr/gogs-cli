9bfb11264d24eeaa7d9ecdb89e8bcef914768070
Reewr <alexander.imenes@gmail.com>
2018-01-28 17:09:57 +0100

    Ignores handled errors

--- commands/issue.js
+++ commands/issue.js
@@ -1,5 +1,7 @@
 'use strict';
-const request  = require('../lib/request');
+const request = require('../lib/request');
+const errors  = require('../lib/errors');
+const NotFound = errors.NotFound;
 
 module.exports = {
   command: 'issue <command>',
@@ -12,15 +14,11 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const res = request.get(`/repos/${username}/${repository}/issues/${id}`);
 
-      res.on('error', (err) => {
-        reject('Failed to retrieve issue due to error: ', err);
+      res.on('error', reject);
+      res.on(404, () => {
+        reject(new NotFound(`Issue #${id} in "${username}/${repository}" not found.`));
       });
-
-      res.on(400, () => reject('The access token you used does not have access'));
-      res.on(401, () => reject('The request that was made was bad'));
-      res.on(404, () => reject(`Issue #${id} in "${username}/${repository}" not found.`));
-      res.on(500, () => reject('An internal server error happened with Gogs'));
-      res.on('success', issue => resolve(issue));
+      res.on('success', resolve);
     });
   },
 
@@ -29,14 +27,9 @@ module.exports = {
     const res = request.get(url);
 
     return new Promise((resolve, reject) => {
-      res.on('error', (err) => reject(err));
-      res.on(400, () => reject('The access token you used does not have access'));
-      res.on(401, () => reject('The request that was made was bad'));
-      res.on(404, () => reject(`Repository "${username}/${repository}" not found.`));
-      res.on(500, () => reject('An internal server error happened with Gogs'));
-      res.on('success', comments => {
-        resolve(comments);
-      });
+      res.on('error', reject);
+      res.on(404, () => reject(new NotFound('Repository', `${username}/${repository}`)));
+      res.on('success', resolve);
     });
   }
 };
