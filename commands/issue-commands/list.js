e9c08d68e49afa4283239bf43bdb62959df72747
Reewr <alexander.imenes@gmail.com>
2018-02-01 00:20:20 +0100

    Adds options to issue list

--- commands/issue-commands/list.js
+++ commands/issue-commands/list.js
@@ -6,7 +6,17 @@ const {InvalidArgument, NotFound} = require('../../lib/errors');
 module.exports = {
   command: 'list <repository>',
   desc   : 'List all issues for a specific repository',
-  builder: {},
+  builder: function(yargs) {
+    return yargs.option('p', {
+      alias   : 'pull-requests',
+      describe: 'Only lists pull requests',
+      boolean : true
+    }).option('n', {
+      alias   : 'no-pull-requests',
+      describe: 'Do not include pull requests',
+      boolean : true
+    });
+  },
   handler: handler.mkHandler(async function(argv) {
     const [username, repository] = argv.repository.split('/');
 
@@ -16,15 +26,33 @@ module.exports = {
     const res = request.get(`/repos/${username}/${repository}/issues`);
 
     return res.then(issues => {
-      const title = `${issues.length} issue(s) was found`;
-      const formattedIssues = issues.map(x => {
-        return `#${x.number} ${x.title} (${x.created_at})`;
-      }).join('\n');
+      const formatted = issues.filter(x => {
+        if (argv.p && !x.pull_request)
+          return false;
+
+        if (argv.n && x.pull_request)
+          return false;
+
+        return true;
+      }).map(x => {
+        const str = `#${x.number} ${x.title} (${x.created_at})`;
+
+        if (x.pull_request)
+          return str + ' (pull request)';
+        return str;
+      });
+
+      const title = !argv.p ?
+        `${issues.length} issue(s) was found` :
+        `${formatted.length} pull request(s) were found`;
+
+      if (formatted.length === 0 && argv.p)
+        return `No pull requests were found on "${username}/${repository}`;
 
       if (issues.length === 0)
-        return `No issues where found on "${username}/${repository}`;
+        return `No issues were found on "${username}/${repository}`;
 
-      return `${title}:\n${formattedIssues}`;
+      return `${title}:\n${formatted.join('\n')}`;
     }).catch(e => {
       if (e instanceof NotFound)
         throw new NotFound('Repository', `${username}/${repository}`);
