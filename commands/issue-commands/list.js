5baf2ed50ec0da1e97ad0b6c1c3bd49b89434ba4
Reewr <alexander.imenes@gmail.com>
2018-02-01 22:10:46 +0100

    Refactors issue list

--- commands/issue-commands/list.js
+++ commands/issue-commands/list.js
@@ -3,6 +3,48 @@ const request = require('../../lib/request');
 const handler = require('../../lib/handler');
 const {InvalidArgument, NotFound} = require('../../lib/errors');
 
+const listForRepository = async function(argv, username, repository) {
+  if (!repository || !username)
+    throw new InvalidArgument('username/repository');
+
+  const res = request.get(`/repos/${username}/${repository}/issues`);
+  const fullname = `${username}/${repository}`;
+
+  return res.then(issues => {
+    const formatted = issues.filter(x => {
+      if (argv.p && !x.pull_request)
+        return false;
+
+      if (argv.n && x.pull_request)
+        return false;
+
+      return true;
+    }).map(x => {
+      const str = `#${x.number} ${x.title} (${x.created_at})`;
+
+      if (x.pull_request)
+        return str + ' (pull request)';
+      return str;
+    });
+
+    const title = !argv.p ?
+      `${issues.length} issue(s) was found on "${fullname}"` :
+      `${formatted.length} pull request(s) were found on ${fullname}`;
+
+    if (formatted.length === 0 && argv.p)
+      return `No pull requests were found on "${fullname}"`;
+
+    if (issues.length === 0)
+      return `No issues were found on "${fullname}"`;
+
+    return `${title}:\n${formatted.join('\n')}`;
+  }).catch(e => {
+    if (e instanceof NotFound)
+      throw new NotFound('Repository', `${fullname}`);
+    throw e;
+  });
+};
+
 module.exports = {
   command: 'list <repository>',
   desc   : 'List all issues for a specific repository',
@@ -20,43 +62,7 @@ module.exports = {
   handler: handler.mkHandler(async function(argv) {
     const [username, repository] = argv.repository.split('/');
 
-    if (!repository || !username)
-      throw new InvalidArgument('username/repository');
-
-    const res = request.get(`/repos/${username}/${repository}/issues`);
-
-    return res.then(issues => {
-      const formatted = issues.filter(x => {
-        if (argv.p && !x.pull_request)
-          return false;
-
-        if (argv.n && x.pull_request)
-          return false;
-
-        return true;
-      }).map(x => {
-        const str = `#${x.number} ${x.title} (${x.created_at})`;
-
-        if (x.pull_request)
-          return str + ' (pull request)';
-        return str;
-      });
-
-      const title = !argv.p ?
-        `${issues.length} issue(s) was found` :
-        `${formatted.length} pull request(s) were found`;
-
-      if (formatted.length === 0 && argv.p)
-        return `No pull requests were found on "${username}/${repository}`;
-
-      if (issues.length === 0)
-        return `No issues were found on "${username}/${repository}`;
-
-      return `${title}:\n${formatted.join('\n')}`;
-    }).catch(e => {
-      if (e instanceof NotFound)
-        throw new NotFound('Repository', `${username}/${repository}`);
-      throw e;
-    });
+    if (username && repository)
+      return await listForRepository(argv, username, repository);
   })
 };
