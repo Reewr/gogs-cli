c9de823c56c2da1882c3598b568b0f03979bca81
Reewr <alexander.imenes@gmail.com>
2018-02-01 22:54:20 +0100

    Adds option of listing repos for user/org

--- commands/repo-commands/list.js
+++ commands/repo-commands/list.js
@@ -1,14 +1,41 @@
 'use strict';
 const request = require('../../lib/request');
+const errors = require('../../lib/errors');
 const mkHandler = require('../../lib/handler').mkHandler;
 const wrap = require('wrap-ansi');
 
+const getRepositoriesForUserOrOrg = function(name) {
+  return request.get(`/users/${name}/repos`)
+    .then(repos => {
+      return repos.filter(x => {
+        const [username] = x.full_name.split('/');
+
+        return username === name;
+      });
+    })
+    .catch(err => {
+      if (err instanceof errors.NotFound ||
+          err instanceof errors.InternalGogsError)
+        return request.get(`/orgs/${name}/repos`);
+      throw err;
+    })
+    .then(repos => repos)
+    .catch(err => {
+      if (err instanceof errors.NotFound)
+        throw new errors.NotFound('Repository', name);
+    });
+};
+
 module.exports = {
-  command: 'list',
+  command: 'list [username_or_orgname]',
   desc   : 'List all available repositories',
   example: 'gogs repo list',
   builder: function(yargs) {
-    return yargs.option('l', {
+    return yargs.positional('username_or_orgname', {
+      describe: 'If present, list out all issues in repositories ' +
+                'for that specific user or organization',
+      type: 'string'
+    }).option('l', {
       alias   : 'list',
       describe: 'Prints out the repositories as a list ' +
                 'instead of columns',
@@ -16,7 +43,12 @@ module.exports = {
     });
   },
   handler: mkHandler(async function(argv) {
-    const repos = await request.get('/user/repos');
+    let repos = [];
+
+    if (argv.username_or_orgname)
+      repos = await getRepositoriesForUserOrOrg(argv.username_or_orgname);
+    else
+      repos = await request.get('/user/repos');
 
     let longestName = 0;
     const formatted = repos
