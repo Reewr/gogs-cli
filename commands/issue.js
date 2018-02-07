8a61ffb4ad2cc91c8c1367135419a488b836c87d
Reewr <alexander.imenes@gmail.com>
2018-02-07 21:00:20 +0100

    Removes unused functions

--- commands/issue.js
+++ commands/issue.js
@@ -1,41 +1,8 @@
 'use strict';
-const request = require('../lib/request');
-const errors  = require('../lib/errors');
-const NotFound = errors.NotFound;
 
 module.exports = {
   command: 'issue <command>',
   desc   : 'Perform operations with issues on Gogs',
   builder: (yargs) => yargs.commandDir('issue-commands').demandCommand(),
-  handler: () => {},
-
-  // extra handlers
-  get: function(username, repository, id) {
-    const res = request.get(`/repos/${username}/${repository}/issues/${id}`);
-
-    return res
-      .then(u => u)
-      .catch(err => {
-        if (err instanceof NotFound)
-          throw new NotFound(
-            `Issue #${id} in "${username}/${repository}" not found.`);
-
-        throw err;
-      });
-  },
-
-
-  getComments: function(username, repository, id) {
-    const url = `/repos/${username}/${repository}/issues/${id}/comments`;
-    const res = request.get(url);
-
-    return res
-      .then(c => c)
-      .catch(err => {
-        if (err instanceof NotFound)
-          throw new NotFound('Repository', `${username}/${repository}`);
-
-        throw err;
-      });
-  }
+  handler: () => {}
 };
